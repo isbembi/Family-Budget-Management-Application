9b13db656be66daacf49d06cc2c4daa422bdce6e
Bermet <iskenderova.bermet19@gmail.com>
2024-12-26 23:28:36 +0600

    mainview done

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -1,4 +1,29 @@
 package view;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MainView {
+    private JFrame frame;
+
+    public MainView() {
+        frame = new JFrame("Family Budget Management");
+        frame.setSize(500, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton userButton = new JButton("Add User");
+        JButton expenseButton = new JButton("Add Expense");
+        JButton goalButton = new JButton("Add Goal");
+
+        userButton.addActionListener(e -> new UserView());
+        expenseButton.addActionListener(e -> new ExpenseView());
+        goalButton.addActionListener(e -> new GoalView());
+
+        frame.setLayout(new FlowLayout());
+        frame.add(userButton);
+        frame.add(expenseButton);
+        frame.add(goalButton);
+
+        frame.setVisible(true);
+    }
 }
